448965d129302f4785528937f5f8ca98c1c4b86d
Jéssica Souza <jessica_schelly@hotmail.com>
2020-10-26 17:22:50 -0300

    added report and lint fixes

--- cypress/integration/pages/ApiGeneratePage.js
+++ cypress/integration/pages/ApiGeneratePage.js
@@ -1,25 +1,25 @@
 class ApiGeneratePage {
-    locators = {
-        numberOfQuestions: "#trivia_amount",
-        generateApiBtn: {
-            element: "button",
-            text: "Generate API URL",
-        },
-        urlValue: 'https://opentdb.com/api.php?amount='
-    }
+  
+  locators = {
+    numberOfQuestions: "#trivia_amount",
+    generateApiBtn: {
+      element: "button",
+      text: "Generate API URL",
+    },
+    urlValue: 'https://opentdb.com/api.php?amount='
+  }
 
-    setNumberOfQuestions(questions) {
-        cy.get(this.locators.numberOfQuestions).clear().type(questions)
-    }
+  setNumberOfQuestions(questions) {
+    cy.get(this.locators.numberOfQuestions).clear().type(questions)
+  }
 
-    clickOnGenerateApiUrl() {
-        cy.get(this.locators.generateApiBtn.element).contains(this.locators.generateApiBtn.text).click()
-    }
+  clickOnGenerateApiUrl() {
+    cy.get(this.locators.generateApiBtn.element).contains(this.locators.generateApiBtn.text).click()
+  }
 
-    checkGeneratedURL(questions) {
-        cy.get(`input[value='${this.locators.urlValue + questions}'`).should('exist')
-    } 
-
+  checkGeneratedURL(questions) {
+    cy.get(`input[value='${this.locators.urlValue + questions}'`).should('exist')
+  } 
 }
 
 export default ApiGeneratePage
--- cypress/integration/pages/SearchDatabasePage.js
+++ cypress/integration/pages/SearchDatabasePage.js
@@ -1,47 +1,47 @@
 
 class SearchDatabasePage {
-    locators = {
-        query: "#query",
-        searchBtn: {
-            element: 'button',
-            text: 'Search'
-        },
-        alertMessageNotfound: {
-            element: 'div'
-        },
-        questionType: '#type',
-        table: ".table",
-        pagination: ".pagination"
-    }
+  
+  locators = {
+    query: "#query",
+    searchBtn: {
+      element: 'button',
+      text: 'Search'
+    },
+    alertMessageNotfound: {
+      element: 'div'
+    },
+    questionType: '#type',
+    table: ".table",
+    pagination: ".pagination"
+  }
 
-    searchInput(question) {
-        cy.get(this.locators.query).type(question)
-    }
+  searchInput(question) {
+    cy.get(this.locators.query).type(question)
+  }
 
-    searchClick() {
-        cy.get(this.locators.searchBtn.element).contains(this.locators.searchBtn.text).click()
-    }
+  searchClick() {
+    cy.get(this.locators.searchBtn.element).contains(this.locators.searchBtn.text).click()
+  }
 
-    validateMessage(message) {
-        cy.get(this.locators.alertMessageNotfound.element).contains(message).should('exist')
-    }
+  validateMessage(message) {
+    cy.get(this.locators.alertMessageNotfound.element).contains(message).should('exist')
+  }
 
-    changeType(category) {
-        cy.get(this.locators.questionType).select(category)
-    }
+  changeType(category) {
+    cy.get(this.locators.questionType).select(category)
+  }
 
-    checkTableSize(quantity) {
-        cy.get(this.locators.table).find('tbody').children().should('have.length', quantity)
-    }
+  checkTableSize(quantity) {
+    cy.get(this.locators.table).find('tbody').children().should('have.length', quantity)
+  }
 
-    paginationExists(){
-        cy.get(this.locators.pagination).should('exist')
-    }
+  paginationExists(){
+    cy.get(this.locators.pagination).should('exist')
+  }
 
-    paginationDoesNotExists(){
-        cy.get(this.locators.pagination).should('not.exist')
-    }
-
+  paginationDoesNotExists(){
+    cy.get(this.locators.pagination).should('not.exist')
+  }
 }
   
 
